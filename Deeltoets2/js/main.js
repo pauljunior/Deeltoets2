2895c190831095547792f1eb3d9f013f996fc097
pauljunior <p.rookhuijzen@gmail.com>
2013-11-03 10:45:24 -0800

    Help, score gaat niet meer omhoog

--- Deeltoets2/js/main.js
+++ Deeltoets2/js/main.js
@@ -8,20 +8,43 @@ var FRISBEE = FRISBEE || {};
 
 	//controller
 	FRISBEE.controller = {
-	    score1: 0,
-		score2: 0,
 		init: function(){
 			FRISBEE.router.init();
-			FRISBEE.game.init();			
-			document.getElementById('finalScore').onclick = function () {
-						var type 		=  'POST',
-						url  		=  'https://api.leaguevine.com/v1/game_scores/',
-						postData 	= JSON.stringify({
-						game_id: '127236',
-						team_1_score: FRISBEE.game.getScore1(),
-						team_2_score: FRISBEE.game.getScore2(),
-						is_final: 'True'
-					});
+			FRISBEE.score.init();
+		}
+	};
+	
+	
+	//schedule met key values etc. (+ een array met naamloze objecten) 
+	FRISBEE.schedule = {
+	};
+	
+	
+	FRISBEE.directives = {
+		schedule: {
+			objects: {
+				track_score: {
+					href: function() {
+						return '#/score/' + this.game_id;
+					}
+				}
+			}
+		}
+	}
+	
+    FRISBEE.score = {
+			score1: 0,
+			score2: 0,
+		init: function () {
+		document.getElementById('finalScore').onclick = function () {
+			var type 		=  'POST',
+			url  		=  'https://api.leaguevine.com/v1/game_scores/',
+				postData 	= JSON.stringify({
+					game_id: FRISBEE.page.render.score.scoreData.id,
+					team_1_score: '1',
+					team_2_score: '1',
+					is_final: 'True'
+				});
 
 				// Create request
 				var xhr = new XMLHttpRequest();
@@ -39,62 +62,39 @@ var FRISBEE = FRISBEE || {};
 				console.log("verzonden");
 			};
 			
+			//Setup plus min
+		
+            score1 = parseInt(document.getElementById('addScore1').innerHTML);
+            score2 = parseInt(document.getElementById('addScore2').innerHTML);
+            document.getElementById('plus1').onclick = function () {
+                score1++;
+                document.getElementById('addScore1').innerHTML = score1;
+                console.log(this.score1);
+            };
+            document.getElementById('plus2').onclick = function () {
+                score2++;
+                document.getElementById('addScore2').innerHTML = score2;
+                console.log(score2);
+            };
+        },
 
-		}
-	
-	};
-	
-
-	
-	
-	
-	
-	
-	//schedule met key values etc. (+ een array met naamloze objecten) 
-	FRISBEE.schedule = function(){
-			poolData: $$.json('https://api.leaguevine.com/v1/game_scores/?tournament_id=19389&access_token=1aa66ab3f7',{}, function(data){
-			FRISBEE.schedule.pools = data;
-			Transparency.render(qwery('[data-route=schedule')[0], FRISBEE.schedule.pools);
-			console.log(FRISBEE.schedule.pools);
-		})
-	};
-	
-
-	
-FRISBEE.game = {
-
-    init: function () {
-        score1 = parseInt(document.getElementById('addScore1').innerHTML);
-        score2 = parseInt(document.getElementById('addScore2').innerHTML);
-        document.getElementById('plus1').onclick = function () {
-            score1++;
+        reset: function() {
+            score1 = 0;
+            score2 = 0;
             document.getElementById('addScore1').innerHTML = score1;
-            console.log(score1);
-        };
-        document.getElementById('plus2').onclick = function () {
-            score2++;
             document.getElementById('addScore2').innerHTML = score2;
-            console.log(score2);
-        };
-    },
+        },
 
-    getScore1: function() {
-        return score1;
-    },
+        getScore1: function () {
+            return score1;
+        },
 
-    getScore2: function() {
-        return score2;
-    },
-
-    reset: function () {
-        score1 = FRISBEE.schedule.pools.objects[0].team_1_score;
-        score2 = FRISBEE.schedule.pools.objects[0].team_2_score;
-        document.getElementById('addScore1').innerHTML = score1;
-        document.getElementById('addScore2').innerHTML = score2;
-    }
-};
-	
-	
+        getScore2: function () {
+            return score2;
+        }
+		
+		
+    };
 
 	
 	
@@ -107,30 +107,40 @@ FRISBEE.game = {
 			]
 	};
 	
+	
+	
+
+	
 	// Router .init wordt hier gemaakt,dit zorgt ervoor dat de nieuwe pagina's aangemaakt worden, hier worden de pagina's genoemd
 	FRISBEE.router = {
 		init: function () {
 	  		routie({
 			    '/schedule': function() {
-			    	FRISBEE.page.schedule();
+			    	FRISBEE.page.render.schedule();
 				},
 
 			    '/ranking': function() {
-			    	FRISBEE.page.ranking();
+			    	FRISBEE.page.render.ranking();
 			    },
-
-			    '/game': function () {
-			    	FRISBEE.page.game();
-			    },
+				
+				'/score/:game_id': function(game_id) { 
+					FRISBEE.page.render.score(game_id);	
+				},
 			    '*': function() {
-			    	FRISBEE.page.schedule();
+			    	FRISBEE.page.render.schedule();
 			    }
 			});
 		},
 		
+		
+		
+		
+		
+		
 		//Hier wordt de function router.change aangemaakt. Deze code zorgt ervoor dat de pagina "ververst"(oude pagina gaat weg, nieuwe komt terug
-		change: function () {
-            var route = window.location.hash.slice(2),
+		change: function (router) {
+	
+             var route = router,
                 sections = qwery('section[data-route]'),
                 section = qwery('[data-route=' + route + ']')[0];  
 
@@ -139,7 +149,7 @@ FRISBEE.game = {
             	for (var i=0; i < sections.length; i++){
             		sections[i].classList.remove('active');
             	}
-            	section.classList.add('active');
+            $$('[data-route='+router+']')[0].classList.add('active');
             }
 
             // Default route
@@ -147,33 +157,44 @@ FRISBEE.game = {
             	sections[0].classList.add('active');
             }
 
+
 		}
 	};
 
+	
+	
+	
+	
 	// Dit zorgt ervoor dat de pages gekoppeld worden aan de .change functie.
-	FRISBEE.page = {
-		schedule: function () {
-			Transparency.render(qwery('[data-route=schedule')[0], FRISBEE.schedule);
-			FRISBEE.router.change();
-			poolData: $$.json('https://api.leaguevine.com/v1/game_scores/?tournament_id=19389&access_token=1aa66ab3f7',{}, function(data){
-			FRISBEE.page.schedule.pools = data;
-				Transparency.render(qwery('[data-route=schedule')[0], FRISBEE.schedule.pools);
-				console.log(FRISBEE.schedule.pools);
-			})
-		},
 
-		ranking: function () {
-			Transparency.render(qwery('[data-route=ranking')[0], FRISBEE.ranking);
-			FRISBEE.router.change();
-		},
 		
-		game: function () {
-		    Transparency.render(qwery('[data-route=game')[0], FRISBEE.game);
-		    FRISBEE.game.reset();
-			FRISBEE.router.change();
+		
+	FRISBEE.page = { 
+		render: { 
+			schedule: function () {
+				$$.get('https://api.leaguevine.com/v1/game_scores/?tournament_id=19389&access_token=1aa66ab3f7',{},function(data){
+					var directives = FRISBEE.directives.schedule;
+					FRISBEE.page.render.schedule.poolData = data;
+					Transparency.render(qwery('[data-route=schedule]')[0], FRISBEE.page.render.schedule.poolData, directives);
+					FRISBEE.router.change('schedule');
+					console.log(FRISBEE.page.render.schedule.poolData);
+				})
+			},
+			ranking: function () {
+				Transparency.render(qwery('[data-route=ranking]')[0], FRISBEE.ranking);
+				FRISBEE.router.change('ranking');
+			},
+		
+			score: function(game_id) {
+				$$.get('https://api.leaguevine.com/v1/games/'+game_id+'/?access_token=6f143cce7e',{}, function(data) {
+					FRISBEE.page.render.score.scoreData = data;
+					Transparency.render(qwery('[data-route=score]')[0], FRISBEE.page.render.score.scoreData);
+					FRISBEE.router.change('score');
+					console.log(FRISBEE.page.render.score.scoreData);
+				});
+			}
 		}
-
-	};
+	}
 
 
 	
@@ -183,6 +204,11 @@ FRISBEE.game = {
 	    FRISBEE.controller.init();
 	    console.log('Init done!')
 	});
+	
+	
+	
+	
+	
 	
 })();
 	
